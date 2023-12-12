99cc98be87963afe4b9fb793e8697e452a4b89d5
Fares-all <faresalsayed1111@gmail.com>
2023-12-12 12:09:18 +0100

    Update Vereinfachung.py

--- Vereinfachung.py
+++ Vereinfachung.py
@@ -11,3 +11,18 @@ c = True
 
 result = boolean_expression(a, b, c)
 print(f"Ergebnis für a={a}, b={b}, c={c}: {result}")
+
+def boolean_expression(a, b, c):
+
+    term1 = (a and b)
+    term2 = (a and c)
+    term3 = (b and (not c))
+    
+    # Neue Variable
+    new_variable = True
+
+    # Neuer Term
+    term4 = (a or new_variable)
+
+    result = term1 or term2 or term3 or term4
+    return result
